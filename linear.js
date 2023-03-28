1e806cd59e66b048735251590c0d34c9a1fcfae9
Veronicandemo <ndemoveronica@gmail.com>
2023-03-28 05:39:16 +0300

    added two linear search practices

--- linear.js
+++ linear.js
@@ -29,3 +29,36 @@ function linearSearch2(arr, target){
 }
 
 console.log(linearSearch2([10,50,30,70,80,20,90,40], 20))
+
+///Search 'theArray' for a specified 'KEy' value 
+
+function theArray(theArray, key){
+    for(let n =0; n < theArray.length; n++){
+        if(theArray[n] === key){ return n}
+    }
+    return -1
+}
+
+console.log(theArray([1,2,3,4,5,6,7,8,9],3))
+console.log(theArray([1,2,3,4,5,6,7,8,9], 14))
+
+
+//
+function linearSearch3(array, n, x){
+    for(let element = 0; element < n; element++){
+        if(array[element] === x){
+            return `Element found at index: ${element}`
+        }
+    }
+    return `element not found`
+}
+let array = [2,4,0,1,9]  //they array on which we are searching 
+let n = array.length  // the array lenght
+let x = 0  //the target
+console.log(linearSearch3(array, n, x))
+
+
+// Given an array Arr of N elements and a integer K. Your task is to return the position of first occurence of K in the given array.
+// Note: Position of first element is considered as 1.
+//Complete the function search() which takes an array arr, two integers n and k, as input parameters and returns an 
+//integer denoting the answer. Return -1 if the number is not found in array. You don't to print answer or take inputs.
